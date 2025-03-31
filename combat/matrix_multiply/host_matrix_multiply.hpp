2113b48470176f5b70b88d87bb2dadfe1952808d
cmd233333 <cmd2333@qq.com>
2025-03-31 19:40:38 +0800

    <feat>(matrix_multiply): impl basic device matrix multiply, not tested yet

--- combat/matrix_multiply/host_matrix_multiply.hpp
+++ combat/matrix_multiply/host_matrix_multiply.hpp
@@ -1,17 +1,19 @@
 #pragma once
 #include <stdlib.h>
+#include <vector>
 
 namespace combat {
 
 namespace matrix_multiply {
 
 template<class T>
-void matrix_multiply_host(T *out, T const* mat1, T const *mat2, std::size_t mat1_height, std::size_t mat1_width, std::size_t mat2_width) {
+std::vector<T> matrix_multiply_host(T const* mat1, T const *mat2, std::size_t mat1_height, std::size_t mat1_width, std::size_t mat2_width) {
     /*
         assume mat1_width == mat2_height
 
         提示: 选择最高效的循环顺序
     */
+    auto out = std::vector<T>(0, mat1_height*mat2_width);
 
     for (std::size_t i=0; i<mat1_height; ++i) {
         for (std::size_t k=0; k<mat1_width; ++k) {
@@ -21,6 +23,8 @@ void matrix_multiply_host(T *out, T const* mat1, T const *mat2, std::size_t mat1
             }
         }
     }
+
+    return out;
 }
 
 } // namespace matrix_multiply
